b08d128c3a9c6079dde9b7a4ec874bb5404b5e86
Laura Simoni <laura.simoni@hostelworld.com>
2023-11-22 17:13:49 +0000

    moving complexity to store

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,51 +1,26 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
-import { TimelinePost, today, thisWeek, thisMonth } from "../posts";
-import { DateTime } from "luxon";
 import TimelineItem from "./TimelineItem.vue";
 import { usePosts } from "../stores/posts";
-
-const periods = ["Today", "This Week", "This Month"] as const;
-type Period = (typeof periods)[number];
-
-const selectedPeriod = ref<Period>("Today");
-
-function selectPeriod(period: Period) {
-  selectedPeriod.value = period;
-}
+import { periods } from "../constants";
 
 const postsStore = usePosts();
-
-const posts = computed<TimelinePost[]>(() => {
-  return [today, thisWeek, thisMonth]
-    .map((post) => {
-      return { ...post, created: DateTime.fromISO(post.created) };
-    })
-    .filter((post) => {
-      if (selectedPeriod.value === "Today") {
-        return post.created >= DateTime.now().minus({ day: 1 });
-      }
-      if (selectedPeriod.value === "This Week") {
-        return post.created >= DateTime.now().minus({ week: 1 });
-      }
-      return post;
-    });
-});
 </script>
 <template>
-  <span>{{ postsStore.foo }}</span>
-  <button @click="postsStore.updateFoo('bar')">update</button>
   <nav class="is-primary panel">
     <span class="panel-tabs">
       <a
         v-for="period of periods"
-        :class="{ 'is-active': period === selectedPeriod }"
+        :class="{ 'is-active': period === postsStore.selectedPeriod }"
         :key="period"
-        @click="selectPeriod(period)"
+        @click="postsStore.setSelectedPeriod(period)"
       >
         {{ period }}</a
       >
     </span>
   </nav>
-  <TimelineItem v-for="post of posts" :key="post.id" :post="post" />
+  <TimelineItem
+    v-for="post of postsStore.filteredPosts"
+    :key="post.id"
+    :post="post"
+  />
 </template>
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,17 +1,50 @@
 import { defineStore } from "pinia"
+import { Period } from "../constants"
+import { Post, today, thisWeek, thisMonth } from "../posts"
+import { DateTime } from "luxon"
 
 interface PostsState {
-    foo: string
+    ids: string[]
+    all: Map<string, Post>
+    selectedPeriod: Period
 }
 
 export const usePosts = defineStore("posts", {
     state: (): PostsState => ({
-        foo: 'foo'
+        ids: [today.id, thisWeek.id, thisMonth.id],
+        all: new Map([
+            [today.id, today],
+            [thisWeek.id, thisWeek],
+            [thisMonth.id, thisMonth]
+        ]),
+        selectedPeriod: "Today"
     }),
 
     actions: {
-        updateFoo(foo: string) {
-            this.foo = foo
+        setSelectedPeriod(period: Period) {
+            this.selectedPeriod = period
         }
     },
+
+    getters: {
+        filteredPosts(state): TimelinePost[] {
+            return state.ids
+                .map((id) => {
+                    const post = state.all.get(id);
+                    if (!post) {
+                        throw new Error(`post with id of ${id} was not found`);
+                    }
+                    return { ...post, created: DateTime.fromISO(post.created) };
+                })
+                .filter((post) => {
+                    if (state.selectedPeriod === "Today") {
+                        return post.created >= DateTime.now().minus({ day: 1 });
+                    }
+                    if (state.selectedPeriod === "This Week") {
+                        return post.created >= DateTime.now().minus({ week: 1 });
+                    }
+                    return post;
+                });
+        }
+    }
 })
