9305e5aa92b31d44569da62d5f211f65881c07fa
Laura Simoni <laura.simoni@hostelworld.com>
2023-11-22 15:25:56 +0000

    section 1 - Basics

---
+++ src/components/Timeline.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { TimelinePost, today, thisWeek, thisMonth } from "../posts";
+import { DateTime } from "luxon";
+import TimelineItem from "./TimelineItem.vue";
+
+const periods = ["Today", "This Week", "This Month"] as const;
+type Period = (typeof periods)[number];
+
+const selectedPeriod = ref<Period>("Today");
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+
+const posts = computed<TimelinePost[]>(() => {
+  return [today, thisWeek, thisMonth]
+    .map((post) => {
+      return { ...post, created: DateTime.fromISO(post.created) };
+    })
+    .filter((post) => {
+      if (selectedPeriod.value === "Today") {
+        return post.created >= DateTime.now().minus({ day: 1 });
+      }
+      if (selectedPeriod.value === "This Week") {
+        return post.created >= DateTime.now().minus({ week: 1 });
+      }
+      return post;
+    });
+});
+</script>
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :class="{ 'is-active': period === selectedPeriod }"
+        :key="period"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}</a
+      >
+    </span>
+  </nav>
+  <TimelineItem v-for="post of posts" :key="post.id" :post="post" />
+</template>
